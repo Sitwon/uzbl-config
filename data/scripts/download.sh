f6edd1d043501aa175cedb1d3cb705409a4a90d4
Ben Mendis <dragonwisard@gmail.com>
2010-04-01 22:32:15 -0400

    Upgrading uzbl for the first time in a long while
    Starting from scratch with the example configs

--- data/scripts/download.sh
+++ data/scripts/download.sh
@@ -3,9 +3,9 @@
 # try some pattern matching on the uri to determine what we should do
 
 # Some sites block the default wget --user-agent..
-GET="wget --user-agent=Firefox"
+GET="wget --user-agent=Firefox --content-disposition --load-cookies=$XDG_DATA_HOME/uzbl/cookies.txt"
 
-dest="$HOME/Downloads"
+dest="$HOME"
 url="$8"
 
 http_proxy="$9"
@@ -16,7 +16,7 @@ test "x$url" = "x" && { echo "you must supply a url! ($url)"; exit 1; }
 # only changes the dir for the $get sub process
 if echo "$url" | grep -E '.*\.torrent' >/dev/null;
 then
-    ( cd "$dest"; eval "$GET" "$url")
+    ( cd "$dest"; $GET "$url")
 else
-    ( cd "$dest"; eval "$GET" "$url")
+    ( cd "$dest"; $GET "$url")
 fi
